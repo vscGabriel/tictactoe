208f54964ee3fef5aa5e29a56774011f028d2bbb
Gabriel <gvscceca@gmail.com>
2022-08-22 16:06:55 -0300

    tictactoe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const emptyBoard = Array(9).fill("");
+  const [board, setBoard] = useState(emptyBoard);
+  const [currentPlayer, setCurrentPlayer] = useState("O");
+  const [winner, setWinner] = useState(null);
+  const handleCellClick = (index) => {
+    if (winner) {
+      alert("Jogo Finalizado");
+      return null;
+    }
+
+    if (board[index] !== "") return null;
+    setBoard(
+      board.map((item, itemIndex) =>
+        itemIndex === index ? currentPlayer : item
+      )
+    );
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  };
+
+  const checkWinner = () => {
+    const possibleWaysToWin = [
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]],
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[8]],
+      [board[0], board[4], board[8]],
+      [board[2], board[4], board[6]],
+    ];
+
+    possibleWaysToWin.forEach((cells) => {
+      if (cells.every((cell) => cell === "O")) setWinner("O");
+      if (cells.every((cell) => cell === "X")) setWinner("X");
+    });
+
+    checkDraw();
+  };
+  const checkDraw = () => {
+    if (board.every((item) => item !== "")) setWinner("D");
+  };
+
+  useEffect(checkWinner, [board]);
+
+  const resetGame = () => {
+    setCurrentPlayer("X");
+    setBoard(emptyBoard);
+    setWinner(null);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1 className="title">Jogo da Velha</h1>
+
+      <div className={`board ${winner ? "game-over" : ""}`}>
+        {board.map((item, itemIndex) => (
+          <div
+            key={itemIndex}
+            className={`cell ${item}`}
+            onClick={() => handleCellClick(itemIndex)}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+      {winner && (
+        <footer>
+          {winner === "D" ? (
+            <h2 className="winner-message">
+              <span>Empate</span>
+            </h2>
+          ) : (
+            <h2 className="winner-message">
+              <span className="winner">{winner}</span> Win!
+            </h2>
+          )}
+
+          <button onClick={resetGame}>Reset game</button>
+        </footer>
+      )}
+    </main>
   );
 }
 
